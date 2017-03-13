3527aaee8d10ef63a411f6017a93914129b25014
Jaime1012 <jaime10herrera@hotmail.com>
2017-03-12 19:30:46 -0500

    Update ejemplo.ino

--- ejemplo.ino
+++ ejemplo.ino
@@ -3,52 +3,45 @@
 ///
 /// by RafaG
 ///
-
+ 
 #include "PuenteH.h"
-
+ 
 /// Definición de pines
 ///
-
-// Motor A
-const int ENA = 5; // Pin PWM
-const int IN1 = 8;
-const int IN2 = 13;
-
-// Motor B
-const int ENB = 6; // Pin PWM
-const int IN3 = 4;
-const int IN4 = 7;
+// Motor derecho
+const int PWMR = 5; // Pin PWM
+const int DIRR = 2;
+// Motor izquierdo
+const int PWML = 6; // Pin PWM
+const int DIRL = 3;
+// Enable motores
+const int ENA = 8;
 ///
 /// Fin de definición de pines
-
+ 
 /// Instancia a clase PuenteH
 ///
-PuenteH puenteH(PuenteH::L298);
+PuenteH puenteH(PWML, DIRL, PWMR, DIRR, ENA);
 ///
 /// Fin de instancia a clase PuenteH
-
+ 
 /// Funciones básicas
 ///
 void setup() {
-    Serial.begin(9600);
-    Serial.println("Inicio de sistema:");
-
-    puenteH.setMotor1(ENA, IN1, IN2);
-    puenteH.setMotor2(ENB, IN3, IN4);
-    puenteH.initMotors();
+ puenteH.initMotors();
 }
-
+ 
 void loop() {
-    puenteH.goForward(200);
-    delay(1000);
-    puenteH.goBackward(200);
-    delay(1000);
-    puenteH.turnLeft(200);
-    delay(1000);
-    puenteH.turnRight(200);
-    delay(1000);
-    puenteH.stop();
-    delay(1000);
+ puenteH.goForward(100);
+ delay(1000);
+ puenteH.goBackward(100);
+ delay(1000);
+ puenteH.turnLeft(100);
+ delay(1000);
+ puenteH.turnRight(100);
+ delay(1000);
+ puenteH.stop();
+ delay(1000);
 }
 ///
 /// Fin funciones básicas
